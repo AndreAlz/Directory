0be52e468ba855ee6b1b01e7e837c67d01825806
André Alzamora Deza <31576039+AndreAlz@users.noreply.github.com>
2018-09-17 11:43:08 -0500

    Update conect.R

--- conect.R
+++ conect.R
@@ -2,11 +2,11 @@ connectMySQL<-function(){
   # Seleccionar driver de trabajo
   m = dbDriver("MySQL")
   # Informacion de la DB
-  myHost <- "10.10.100.192"
-  myUsername = "root"
-  myDbname = "db_emple_general"
-  myPort = 3306
-  myPassword = "Sysserver02"
+  myHost <- "IP"
+  myUsername = "USER"
+  myDbname = "DB NAME"
+  myPort = "PORT"
+  myPassword = "PASSWORD"
   #Creacion del obj. de conexion
   con = dbConnect(m, user= myUsername, host= myHost, password= myPassword, dbname= myDbname, port= myPort)
   return(con)}
